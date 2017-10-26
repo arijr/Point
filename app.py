49d4e2c3c4915a1488b0273d3e43c153a1404104
arijr <arivaniltonjr@gmail.com>
2017-10-26 12:06:29 -0300

    exemplo de uso do metodo __eq__ para comparação de estado(valor das variáveis), ao inves de identidade (posição de memória)

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ from model.point import Point
 def main(args = []):
 
     p1 = Point(3,5)
-    p2 = Point(2,4)
+    p2 = Point(3,5)
 
     print(p1+p2)
+
+    if(p1 == p2):
+        print("São Iguais")
+    else:
+        print("são diferentes")
 
 if __name__ == "__main__":
     main()
--- model/point.py
+++ model/point.py
@@ -6,7 +6,22 @@ class Point:
     def __add__(self,outro):
         p1 = self.x + outro.x
         p2 = self.y + outro.y
-        return self.__str__(p1,p2)
+        Pn = Point(p1,p2)
 
-    def __str__(self,p1,p2):
-        return "Pn("+str(p1)+","+str(p2)+")"
+        return Pn
+        # return self.__str__(p1,p2)
+
+    def __str__(self): #__str__(self,p1,p2)
+
+        return "Pn(%.2f,%.2f)"%(self.x,self.y)
+
+        # return "Pn("+str(self.x)+","+str(self.y)+")"
+
+    # def __str__(self,p1,p2):
+    #     return "Pn("+str(p1)+","+str(p2)+")"
+
+    # def __cmp__(self, outro):
+    #     return cmp((self.x,self.y),(outro.x,outro.y))
+
+    def __eq__(self, outro):
+        return self.x == outro.x and self.y == outro.y
