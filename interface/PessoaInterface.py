6e328132e7a852da4b5359ed29dfe5b042b30a3e
arijr <arivaniltonjr@gmail.com>
2017-11-16 12:24:20 -0300

    tratamento de erro e criação de interface

---
+++ interface/PessoaInterface.py
@@ -0,0 +1,10 @@
+class PessoaInterface:
+
+    def cadastrar(self):
+        pass
+
+    def atualizar(self,Pessoa):
+        pass
+
+    def remover(self,id):
+        pass
---
+++ model/Pessoa.py
@@ -0,0 +1,34 @@
+from interface.PessoaInterface import PessoaInterface
+from datetime import date
+
+
+class Pessoa(PessoaInterface):
+
+    def __init__(self, nome, email, nascimento):
+        self.nome = nome
+        self.email = email
+        self.nascimento = nascimento
+
+    def cadastrar(self):
+        print('Cadastrando...\n')
+        p = Pessoa(self.nome,self.email,self.nascimento)
+
+        return p
+
+    def __str__(self):
+        pessoa = 'Nome: %s \nEmail: %s \nNascimento: %s' %(self.nome,self.email,self.nascimento)
+
+        return pessoa
+
+    def __eq__(self, other):
+        return (self.nome == other.nome) \
+            and (self.email == other.email) \
+            and (self.nascimento == other.nascimento)
+
+p = Pessoa('Ari', 'ari@teste', date(1986,12,22))
+p2 = Pessoa('Jose','jose@mail',date.today())
+
+print(p)
+print(p2)
+
+print(p==p2)
