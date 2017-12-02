ee0e99c5209e9847a00b3e37dc7cb00065959b04
Arivanilton Araújo Júnior <arijr@NoteOne>
2017-12-02 12:49:46 -0300

    Novas alterações no app.py

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from datetime import date
 import json
 
 def listarParticipantes():
-
+    participantes = []
     #leitura do arquivo
     lista_part = open("participantes.json",'r',encoding="utf8")
-
     #Parser do JSON em objeto
     jsonObjs = json.loads(lista_part.read())
 
@@ -18,17 +17,34 @@ def listarParticipantes():
         email = jsonObj["email"]
         data = jsonObj["nascimento"].split("-")
         nascimento = date(int(data[0]),int(data[1]),int(data[2]))
-        participante = Participante(nome,email,nascimento)
+        participante = Participante(nome,nascimento,email)
         print(participante)
 
+
 def adicionarParticipantes():
+    #leitura do arquivo
+    lista_part = open("participantes.json",'r',encoding="utf8")
+
+    #Parser do JSON em objeto
+    jsonObjs = json.loads(lista_part.read())
+
     print("\n\n ======== Novo Participante: ======== \n\n")
     nome = input("Digite o nome:")
     nascimento = input("Digite a data de nascimento:")
     email = input("Digite o email:")
+    participante = Participante(nome,nascimento,email)
+
+    partJson = {}
+    partJson['nome'] = participante.nome
+    partJson['email'] = participante.email
+    partJson['nascimento'] = participante.nascimento
+    jsonObjs.append(partJson)
+
+    with open('participantes.json', 'w', encoding='utf-8') as lista_part:
+        json.dump(jsonObjs, lista_part, ensure_ascii=False, indent=4)
 
     print("\n\n *Novo participante criado.* \n\n")
-    participante = Participante(nome,nascimento,email)
+
 
     return participante
 
@@ -50,6 +66,7 @@ def main(args = []):
 
             if opcao == 1:
                 listarParticipantes()
+                exibirMenu()
             elif opcao == 2:
                 adicionarParticipantes()
                 exibirMenu()
